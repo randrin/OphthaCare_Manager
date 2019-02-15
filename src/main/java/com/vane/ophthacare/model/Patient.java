d865cfec9103e64c77d73008c8bc19bc00b730f1
@randrin <nzeukangrandrin@gmail.com>
2019-02-15 17:32:56 +0100

    Configuration environement

---
+++ src/main/java/com/vane/ophthacare/model/Patient.java
@@ -0,0 +1,9 @@
+package com.vane.ophthacare.model;
+
+import java.io.Serializable;
+
+public class Patient implements Serializable {
+
+	private static final long serialVersionUID = -7687560243779956203L;
+
+}
