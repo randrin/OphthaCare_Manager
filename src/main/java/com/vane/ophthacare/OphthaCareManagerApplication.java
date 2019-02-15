da615dc73b3cb9eba57d560829d76fdae119ddf8
@randrin <nzeukangrandrin@gmail.com>
2019-02-15 17:20:33 +0100

    Initial commit

---
+++ src/main/java/com/vane/ophthacare/OphthaCareManagerApplication.java
@@ -0,0 +1,14 @@
+package com.vane.ophthacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OphthaCareManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OphthaCareManagerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/vane/ophthacare/OphthaCareManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vane.ophthacare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OphthaCareManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
