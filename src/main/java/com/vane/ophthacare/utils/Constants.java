80009202581c7d8f63620f6ea408d279c2e8d3a1
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2019-05-28 23:07:33 +0200

    Correction log/Report admin

--- src/main/java/com/vane/ophthacare/utils/Constants.java
+++ src/main/java/com/vane/ophthacare/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final String SUCCESS = "Success";
 	public static final String FAILED = "Failed";
 	public static final String BEGIN = "PROCESSING ...";
-	public static final String END = "END";
+	public static final String END = "END ...";
 	public static final String NEW_ID = "NEW";
 	public static final String MODEL_UNKNOWN = "Unknown";
 }
